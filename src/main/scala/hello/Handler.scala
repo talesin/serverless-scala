04fa864890cb619814dd62f8422e3dcf3c67fe33
Jeremy Clough <jeremy@talesin.net>
2016-10-20 12:42:55 +1100

    sample working

--- src/main/scala/hello/Handler.scala
+++ src/main/scala/hello/Handler.scala
@@ -5,7 +5,8 @@ import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 class Handler extends RequestHandler[Request, Response] {
 
 	def handleRequest(input: Request, context: Context): Response = {
-		return new Response("Go Serverless v1.0! Your function executed successfully!", input)
+		//Response("Go Serverless v1.0! Your function executed successfully!", input)
+		Response(200, "{\"message\":\"Go Serverless v1.0! Your function executed successfully!\"}")
 	}
 
 }
--- src/main/scala/hello/Response.scala
+++ src/main/scala/hello/Response.scala
@@ -2,4 +2,5 @@ package hello
 
 import scala.beans.BeanProperty
 
-case class Response(@BeanProperty message: String, @BeanProperty request: Request)
+//case class Response(@BeanProperty message: String, @BeanProperty request: Request)
+case class Response(@BeanProperty statusCode: Int, @BeanProperty body: String)
